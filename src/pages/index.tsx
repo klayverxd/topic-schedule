33e0d7f92f3882cc1af53a0dd860910286b4dc3b
Klayver Ximenes <klayverx@gmail.com>
2023-08-19 13:57:09 -0300

    style: add styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Image from 'next/image';
 const inter = Inter({ subsets: ['latin'] })
 
 interface Schedule {
@@ -16,14 +17,14 @@ interface SchedulesType {
 
 const schedules: SchedulesType = {
   coreauToSobral: {
-    week: ['08:00', '10:00', '12:00'],
-    saturday: ['09:00', '11:00', '13:00'],
-    sunday: ['10:00', '12:00', '14:00'],
+    week: ['05:40', '05:55', '06:10', '07:10', '07:50', '08:30', '09:20', '10:10', '11:00', '11:50', '12:40', '13:30', '15:00'],
+    saturday: ['05:50', '06:10', '06:50', '07:30', '07:50', '08:30', '09:20', '10:10', '11:00', '11:50', '12:10', '13:00', '13:20', '15:00'],
+    sunday: ['06:00', '13:00'],
   },
   sobralToCoreau: {
-    week: ['09:00', '11:00', '13:00'],
-    saturday: ['10:00', '12:00', '14:00'],
-    sunday: ['11:00', '13:00', '15:00'],
+    week: ['07:30', '08:15', '09:50', '10:20', '11:00', '11:40', '12:30', '13:20', '14:10', '15:00', '15:50', '16:40', '17:30'],
+    saturday: ['07:30', '08:10', '08:50', '09:30', '10:10', '10:50', '11:40', '12:30', '13:20', '14:10', '15:00', '15:50', '16:40', '17:30'],
+    sunday: ['07:30', '14:30'],
   },
 };
 
@@ -37,45 +38,56 @@ export default function Home() {
   const renderHorarios = (direction: keyof SchedulesType, selectedDay: keyof Schedule) => (
     <ul>
       {schedules[direction][selectedDay].map((horario, index) => (
-        <li key={index}>{horario}</li>
+        <li key={index} className={styles.rowList}>{horario}</li>
       ))}
     </ul>
   );
 
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        Horários Topic Coreaú/Sobral
-      </header>
+      <div className={styles.overlay}>
+        <header className={styles.header}>
+          <h2>
+            Horários Topic Coreaú/Sobral
+          </h2>
+        </header>
 
-      <div className={styles.main}>
-        <div className={styles.headerSchedules}>
-          <label htmlFor="daySelect">Selecione o período: </label>
-          <select id="daySelect" value={selectedDay} onChange={handleSelectChange}>
-            <option value="week">Semana</option>
-            <option value="saturday">Sábado</option>
-            <option value="sunday">Domingo</option>
-          </select>
-        </div>
+        <main className={styles.main}>
+          <div className={styles.headerSchedules}>
+            <label htmlFor="daySelect">Selecione o período: </label>
+            <select id="daySelect" className={styles.select} value={selectedDay} onChange={handleSelectChange}>
+              <option value="week">Semana</option>
+              <option value="saturday">Sábado</option>
+              <option value="sunday">Domingo</option>
+            </select>
+          </div>
 
-        <div className={styles.containerColumns}>
-          <div className={styles.column}>
-            <h3 className={styles.textColumn}>Coreaú - Sobral</h3>
-            {renderHorarios('coreauToSobral', selectedDay)}
+          <div className={styles.containerColumns}>
+            <div className={styles.column}>
+              <h3 className={styles.textColumn}>Coreaú - Sobral</h3>
+              {renderHorarios('coreauToSobral', selectedDay)}
+            </div>
+            <div className={styles.column}>
+              <h3 className={styles.textColumn}>Sobral - Coreaú</h3>
+              {renderHorarios('sobralToCoreau', selectedDay)}
+            </div>
           </div>
-          <div className={styles.column}>
-            <h3 className={styles.textColumn}>Sobral - Coreaú</h3>
-            {renderHorarios('sobralToCoreau', selectedDay)}
+        </main>
+
+
+        <footer className={styles.footer}>
+          <Image
+            src="https://imagepng.org/wp-content/uploads/2017/08/whatsapp-icone-2.png"
+            width={30}
+            height={30}
+            alt="WhatsApp"
+          />
+          <div className={styles.footerContent}>
+            <a href='https://chat.whatsapp.com/CXtM5hqrYL4AmHn5FbLVIl' target='_blank'>Grupo Universitários Noite</a>
+            <a href='https://chat.whatsapp.com/Co85HIYs6bs1jJX4Mn9msZ' target='_blank'>Grupo Universitários Manhã</a>
           </div>
-        </div>
+        </footer>
       </div>
-
-
-      <footer className={styles.footer}>
-        <a href='https://chat.whatsapp.com/CXtM5hqrYL4AmHn5FbLVIl' target='_blank'>Grupo Universitários Noite</a>
-        <a href='https://chat.whatsapp.com/Co85HIYs6bs1jJX4Mn9msZ' target='_blank'>Grupo Universitários Manhã</a>
-      </footer>
-
     </div>
   )
 }
